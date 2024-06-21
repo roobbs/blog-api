c956284192521ac65ca5c6d3b70d0da6ce65e62f
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-06-21 12:28:48 -0600

    Created controller to add a comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,10 +2,40 @@ const Post = require("../models/posts");
 const Comment = require("../models/comment");
 const BlogUserUser = require("../models/blogUser");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
-exports.createComment = asyncHandler(async (req, res, next) => {
-  // const posts = await Post.find({ published: true }).sort({ date: 1 }).exec();
+exports.createComment = [
+  body("message", "Message must be at least 6 characters")
+    .trim()
+    .isLength({ min: 6 })
+    .escape(),
 
-  // res.json(posts);
-  res.send("comment");
-});
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { message, userId, postId } = req.body;
+
+    try {
+      const comment = new Comment({
+        author: userId,
+        post: postId,
+        date: new Date(),
+        text: message,
+      });
+
+      await comment.save();
+
+      res.status(201).json({
+        success: true,
+        msg: "Comment created successfully",
+        comment,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }),
+];
--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,7 @@ const CommentSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "BlogUser", required: true },
   post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   date: { type: Date, required: true },
-  text: { type: String, required: true, minLength: 20 },
+  text: { type: String, required: true, minLength: 6 },
 });
 
 module.exports = mongoose.model("Comment", CommentSchema);
